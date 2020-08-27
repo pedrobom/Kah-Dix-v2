8046695b1742177aec323922c5b3f5b3ab1164e3
Vitor Sandes <sandesvitor@gmail.com>
2020-08-27 17:08:10 -0300

    Move App.jsxand App.css to the the folder 'main'

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App.jsx'
-
+import App from './main/App.jsx'
 ReactDOM.render(<App />, document.querySelector("#root"));
--- client/src/main/App.jsx
+++ client/src/main/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css'
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Join from "./Components/Join/Join.js"
-import GameRoom from "./Components/GameRoom/GameRoom.jsx"
+import Join from "../Components/Join/Join.js"
+import GameRoom from "../Components/GameRoom/GameRoom.jsx"
 
 const App = () => (
     <Router>
