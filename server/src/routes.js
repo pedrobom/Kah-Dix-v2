7943bd1af8da3bf4d5aa756fa28965715d7e7ca4
Vitor Sandes <sandesvitor@gmail.com>
2020-09-19 15:12:49 -0300

    Installed ts-node and typescript compiler as devDependencies/run tsc --ininit

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 
-const RoomController = require('./POSTGRESQL/controllers/RoomController')
 const routes = express.Router();
 
-//#####TESTE DE POST (USANDO INSOMNIA)###
-routes.post('/rooms', RoomController.store)
-routes.get('/rooms', RoomController.list)
-//#######################################
 
 routes.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
