249ad15b103eb4837acee58124bb5a4da5bacee9
Guilherme Pim <pimguilherme@gmail.com>
2020-09-20 19:38:02 -0300

    Adicionando uns icone

--- client/src/Components/GameRoom/Sidebar/PlayerList/PlayerList.jsx
+++ client/src/Components/GameRoom/Sidebar/PlayerList/PlayerList.jsx
@@ -20,6 +20,7 @@ export default React.memo(function PlayerList() {
             let isSessionPlayer = session && session.user && session.user.id == player.id
             let isHostPlayer = player.id == roomData.host.id
             let isDisconnected = player.isDisconnected
+            let isCurrentPlayer = currentPlayer.id == player.id
 
             // Vamos indicar o que o jogador precisa fazer nesse estado..
             var playerStateIcon = ''
@@ -55,7 +56,9 @@ export default React.memo(function PlayerList() {
 
             return <tr key={index} className={'player ' + (isSessionPlayer ? "thisIsMe " : "") + (isDisconnected ? 'disconnected': '')}>
                 <td className='playerName'>
+                    { isCurrentPlayer ? <i title='É o turno dessa pessoa escolher a frase :)' className='fas currentPlayerIcon fa-long-arrow-right'></i> : ''}
                     <span className='name'>{player.name} </span>
+                    { isSessionPlayer ? <i title='Esse/a é você :)' className='fas sessionPlayerIcon fa-user'></i> : ''}
                     { isHostPlayer ? <i title='Essa pessoa aqui é dona da sala.. fica esprt' className='fas hostPlayerIcon fa-star'></i> : ''}
                     { isDisconnected ? <i title='Essa pessoa foi dar um pulo no banheiro e já volta' className='fas playerDisconnectedIcon fa-toilet-paper'></i> : ''}
                 </td>
