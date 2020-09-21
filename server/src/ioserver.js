26d6dbc944495d3c0cdfca44cde9aaf67e241b3d
Guilherme Pim <pimguilherme@gmail.com>
2020-09-20 22:51:55 -0300

    server com front

--- server/src/ioserver.js
+++ server/src/ioserver.js
@@ -42,6 +42,9 @@ server.on('request', (req, res) => {
 //   oldWriteHead.apply(this, arguments)
 // }
 
+// Servir o front aqui mesmo :)
+app.use('/', express.static('../client/build'))
+
 // Outros modulos do servidor
 app.use(cors());
 app.use(routes)
--- server/src/routes.js
+++ server/src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 
-routes.get("/", (req, res) => {
+routes.get("/healthcheck", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
 });
 
