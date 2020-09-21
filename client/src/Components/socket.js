7fb629265a0b9030b9fd748ed377a6ffd835663e
Guilherme Pim <pimguilherme@gmail.com>
2020-09-20 21:41:57 -0300

    api link changed

--- client/src/Components/socket.js
+++ client/src/Components/socket.js
@@ -1,5 +1,6 @@
 import openSocket from 'socket.io-client'
 import React from "react";
+import Constants from '../Constants'
 
-export const socket = openSocket("http://localhost:5000/");
+export const socket = openSocket(Constants.apiAddress);
 export default socket;
