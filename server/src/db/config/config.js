c166c8b3485e0de4565aebbcc45505e58a0d14c1
Guilherme Pim <pimguilherme@gmail.com>
2020-10-04 15:31:55 -0300

    adicionando config de nome de DB e .env_template para criar .env(s)

--- server/src/db/config/config.js
+++ server/src/db/config/config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: 'postgres',
   define: {
